ac716c9e0a68721ab45d1b25729193da5d4ca2cd
zoomstv <danielhampel.danham@gmail.com>
2023-12-15 16:03:19 +0100

    added mod version 1

---
+++ 24HourClock/Patches/HUDManagerPatch.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using TMPro;
+
+namespace _24HourClock.Patches
+{
+    [HarmonyPatch(typeof(HUDManager))]
+    internal class HUDManagerPatch
+    {
+        public static HUDManagerPatch Instance;
+
+        private static TextMeshProUGUI clockNumber;
+
+        void Awake()
+        {
+            if(Instance == null)
+            {
+                Instance = this;
+            }
+        }
+
+        [HarmonyPatch("SetClock")]
+        [HarmonyPostfix]
+        static void SetClockPatch(float timeNormalized, float numberOfHours, bool createNewLine = true)
+        {
+            clockNumber = HUDManager.Instance.clockNumber;
+
+            int minutes = (int)(timeNormalized * (60f * numberOfHours)) + 360;
+
+            int hours = minutes / 60;
+            minutes = minutes % 60;
+
+            string timeString = hours.ToString("00") + ":" + minutes.ToString("00");
+
+            clockNumber.text = timeString;
+        }
+    }
+}
---
+++ 24HourClock/Plugins.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using _24HourClock.Patches;
+
+namespace _24HourClock
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class PluginBase : BaseUnityPlugin
+    {
+        private const string modGUID = "zoomstv.24HourClock";
+        private const string modName = "24HourClock";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static PluginBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("24HourClock has awakened");
+
+            harmony.PatchAll(typeof(PluginBase));
+            harmony.PatchAll(typeof(HUDManagerPatch));
+        }
+    }
+}
